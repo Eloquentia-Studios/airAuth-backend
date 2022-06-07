385338937f27ba8a016b01509b9ff6473d520dee
Esaias Tilly <esaias@tillys.se>
2022-06-07 17:04:00 +0200

    Created express server and listens on port 8080.
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

---
+++ src/services/express.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import type { Express } from 'express'
+
+/**
+ * Create a new Express server and run the automatic setup.
+ *
+ * @returns Express instance.
+ */
+export const createServer = (): Express => {
+  // Create a new express server.
+  const app = express()
+  setupGlobalMiddleware(app)
+  setupRoutes(app)
+
+  return app
+}
+
+/**
+ * Sets up the global Express middlewares.
+ *
+ * @param app Express instance.
+ */
+const setupGlobalMiddleware = (app: Express): void => {
+  app.use(express.json())
+}
+
+/**
+ * Sets up the Express routes.
+ *
+ * @param app Express instance.
+ */
+const setupRoutes = (app: Express): void => {}
