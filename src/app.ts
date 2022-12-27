dc55f87a3ecafbf50b37418c7a7b351a7fab0541
Esaias Cronelius <esaias@cronelius.se>
2022-12-27 21:55:13 +0100

    Automatic backup configuration
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import createDummyData from './lib/createDummyData.js'
 import logDebug from './lib/logDebug.js'
-import { writeDefaultConfig } from './services/config.js'
 import { createServer } from './services/express.js'
 import { loadKeys } from './services/jwt.js'
 import { initSync } from './services/sync.js'
 
 logDebug('Starting server...')
 
-// Write the default configuration file.
-const configPath = process.env.CONFIG_PATH || './config/config.json'
-writeDefaultConfig(configPath)
 loadKeys()
 
 // Create dummy data.
