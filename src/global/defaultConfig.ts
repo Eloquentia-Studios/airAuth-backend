9b761c40b8f12c76e797e3919edc8c33ef68106e
Esaias Cronelius <esaias@cronelius.se>
2022-12-29 12:26:41 +0100

    Repair configurations on start. (PROGRAM DOES NOT START)
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

--- src/global/defaultConfig.ts
+++ src/global/defaultConfig.ts
@@ -8,7 +8,7 @@ export const defaultConfig: ServerConfigurationInput = {
     keep: 10,
     secret: 'THIS-SHOULD-BE-RANDOMLY-GENERATED'
   },
-  sync: {
+  websocket: {
     enabled: false,
     server: {
       name: 'SERVER-NAME',
@@ -17,12 +17,17 @@ export const defaultConfig: ServerConfigurationInput = {
     servers: [
       {
         name: 'SECOND-SERVER-NAME',
-        address: 'server.two:7070'
+        address: 'server.two:7070',
+        sync: false
       }
     ],
     ssl: false,
+    tryConnectInterval: 5
+  },
+  sync: {
+    enabled: false,
     fullSyncInterval: 30,
-    tryConnectInterval: 5,
+
     secret: 'THIS-SHOULD-BE-RANDOMLY-GENERATED'
   },
   debug: false
@@ -39,27 +44,35 @@ export const defaultConfigComments = {
     secret:
       'A secret to encrypt the backups with. This should be generated using a tool such as: https://www.random.org/strings/?num=5&len=20&digits=on&upperalpha=on&loweralpha=on&format=plain'
   },
-  sync: {
-    _: 'Synchronization configuration.',
-    enabled:
-      'Enables or disables the server syncronization service. DEFAULT: false',
+  websocket: {
+    _: 'Websocket configuration.',
+    enabled: 'Enables or disables the websocket server. DEFAULT: false',
     server: {
       _: 'Name of the local server. DEFAULT: SERVER-NAME',
       name: 'Name of the local server. DEFAULT: SERVER-NAME',
       port: 'Port to use for websocket server for connection between servers. DEFAULT: 7070'
     },
     servers: {
-      _: 'List of remote servers to synchronize with. Currently only supports one server.',
+      _: 'List of remote servers to connect to. Currently only supports one server.',
       0: {
         name: 'Name of the server to synchronize with. DEFAULT: SECOND-SERVER',
-        address: 'Websocket connection address. DEFAULT: http://server.two:7070'
+        address:
+          'Websocket connection address. DEFAULT: http://server.two:7070',
+        sync: 'Enables or disables synchronization with this server. If field not defined, will assume default value. DEFAULT: false'
       }
     },
     ssl: 'This indicates if websocket connections should use SSL by default. This is overridden if either `ws://` or `wss://` is used in the server address. DEFAULT: false',
+    tryConnectInterval:
+      'The time between attempts to connect to remote servers. UNIT: min DEFAULT: 5'
+  },
+  sync: {
+    _: 'Synchronization configuration.',
+    enabled:
+      'Enables or disables the server syncronization service. DEFAULT: false',
+
     fullSyncInterval:
       'The time between full database syncs to run during runtime. Zero to disable. UNIT: min DEFAULT: 30',
-    tryConnectInterval:
-      'The time between attempts to connect to remote servers. UNIT: min DEFAULT: 5',
+
     secret:
       'A secret which should be shared between all servers. This should be generated using a tool such as: https://www.random.org/strings/?num=5&len=20&digits=on&upperalpha=on&loweralpha=on&format=plain'
   },
